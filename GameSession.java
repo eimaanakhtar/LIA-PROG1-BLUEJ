1bd4727c049c233c5135954e8ba993bbe87cfe3f
Eimaan Akhtar <eimaanakhtar12@gmail.com>
2026-04-29 11:39:31 -0400

    Initial sharing of project

---
+++ GameSession.java
@@ -0,0 +1,33 @@
+
+/**
+ * Write a description of class GameSession here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class GameSession
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class GameSession
+     */
+    public GameSession()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
